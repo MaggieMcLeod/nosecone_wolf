da2b29adcba9fca3d5a02409b0d5bf9791fe72cd
Lukepw23 <116756110+Lukepw23@users.noreply.github.com>
2025-02-11 17:13:41 -0500

    Create main_V1.py

---
+++ main_V1.py
@@ -0,0 +1,76 @@
+### UNTESTED CODE ###
+### records video with USB camera and PI camera simultaneously
+
+import cv2
+from picamzero import Camera
+import serial
+
+class USB_camera_control:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(1, cv2.CAP_V4L2)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.out = cv2.VideoWriter('output.avi', self.fourcc, 20.0, (640, 480))
+
+    def record_frame(self):
+        _, frame = self.cap.read()
+
+        self.out.write(frame)
+
+        # # Display the frame
+        # cv2.imshow('Recording', frame)
+
+    def stop(self):
+        self.cap.release()
+        self.out.release()
+        cv2.destroyAllWindows()
+
+class PI_camera_control:
+    def __init__(self):
+        self.cam = Camera()
+
+    def record(self):
+        self.cam.record_video(f"./", duration=120)
+
+    def preview(self):
+        self.cam.start_preview()
+
+
+def main():
+    usb = USB_camera_control()
+    pi = PI_camera_control()
+    recording = False
+    piStarted = False
+
+    ser = serial.Serial(
+        port='/dev/ttyS0',
+        baudrate = 9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
+
+    pi.preview()
+
+    while True:
+        x = ser.readline()
+        if x != b'':
+            if (b"start" in x) and (not recording):
+                recording = True
+
+            elif (b"stop" in x) and (recording):
+                recording = False
+                usb.stop()
+                piStarted = False
+
+
+        if recording:
+            usb.record_frame()
+            if not piStarted:
+                pi.record()
+                piStarted = True
+
+    
+    
